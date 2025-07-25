5a904d15671a846213013dbec86c2c49f0b3256d
Elian <alfagelion@gmail.com>
2025-07-25 17:52:35 -0600

    Intento, Index

---
+++ src/main/java/Risosu/it/EGDDTheMovieDB25Julio/EgddTheMovieDb25JulioApplication.java
@@ -0,0 +1,13 @@
+package Risosu.it.EGDDTheMovieDB25Julio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgddTheMovieDb25JulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgddTheMovieDb25JulioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Risosu/it/EGDDTheMovieDB25Julio/EgddTheMovieDb25JulioApplicationTests.java
@@ -0,0 +1,13 @@
+package Risosu.it.EGDDTheMovieDB25Julio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgddTheMovieDb25JulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
